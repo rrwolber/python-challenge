49d6852a83e709d4675ae18cb8082b312936a729
rrwolber <becca.wolber@gmail.com>
2018-04-26 20:21:32 -0700

    Update PyParagraph.py

--- PyParagraph.py
+++ PyParagraph.py
@@ -1,20 +1,17 @@
 ##Paragraph reader that gives you info on the word and sentence content of given paragraph##
 
-#fyi in homework example output I think it's counting a dash as a word... mine for the sample paragraph shows one less word because I stripped the dash doing my own regex research
-
-
 import re
 
 f = open('paragraph.txt','r')
 
 paragraph = str(f.read())
-AllSentences = re.split('[.?!]+', paragraph)               #Question: re command given in hw didn't print properly, but need to do left bound for 
+AllSentences = re.split('[.?!]+', paragraph)        #removing full stop punctiation to separate out sentences       
 NoPunct = []
 for sentence in AllSentences:
     if not sentence:
         AllSentences.remove(sentence)
     else:
-        NoPunct = NoPunct + re.split('[,;:"-]+',sentence)  #removing non-stop punctuation and punctuation at end of paragraph... needed for dash character, but I put all the others in because words shouldn't have punctuation attached
+        NoPunct = NoPunct + re.split('[,;:"-]+',sentence)  #removing non-stop punctuation and punctuation at end of paragraph
 
 
 AllWords = []                                              #removing spaces
